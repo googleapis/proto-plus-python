c9b20cc416ea874d7009800f7a5d031e0687039d
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-04-16 00:58:25 -0400

    chore: update workaround to make sic work

--- setup.py
+++ setup.py
@@ -17,6 +17,21 @@ import os
 
 import setuptools
 
+# Disable version normalization performed by setuptools.setup()
+try:
+    # Try the approach of using sic(), added in setuptools 46.1.0
+    from setuptools import sic
+except ImportError:
+    # Try the approach of replacing packaging.version.Version
+    sic = lambda v: v
+    try:
+        # setuptools >=39.0.0 uses packaging from setuptools.extern
+        from setuptools.extern import packaging
+    except ImportError:
+        # setuptools <39.0.0 uses packaging from pkg_resources.extern
+        from pkg_resources.extern import packaging
+    packaging.version.Version = packaging.version.LegacyVersion
+
 version = "1.18.1"
 
 PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
@@ -26,7 +41,7 @@ with io.open(os.path.join(PACKAGE_ROOT, "README.rst")) as file_obj:
 
 setuptools.setup(
     name="proto-plus",
-    version=setuptools.sic(version),
+    version=sic(version),
     license="Apache 2.0",
     author="Google LLC",
     author_email="googleapis-packages@google.com",
