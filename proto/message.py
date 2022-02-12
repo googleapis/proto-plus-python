3ea8150e41f5b6652eda8992984f8ae44f064d6d
Anthonios Partheniou <partheniou@google.com>
2022-02-12 02:41:11 +0000

    fix: mitigate collisions in field names

--- proto/message.py
+++ proto/message.py
@@ -524,6 +524,16 @@ class Message(metaclass=MessageMeta):
         # coerced.
         marshal = self._meta.marshal
         for key, value in mapping.items():
+            # Underscores may be appended to field names
+            # that collide with python or proto-plus keywords.
+            # In case a key only exists with a `_` suffix, coerce the key
+            # to include the `_` suffix. Is not possible to
+            # natively define the same field with a trailing underscore in protobuf.
+            # See related issue
+            # https://github.com/googleapis/python-api-core/issues/227
+            if key not in self._meta.fields and f"{key}_" in self._meta.fields:
+                key = f"{key}_"
+
             try:
                 pb_type = self._meta.fields[key].pb_type
             except KeyError:
