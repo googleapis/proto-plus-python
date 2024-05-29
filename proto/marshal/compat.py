a0242df358d88521c7bf661076997b9d8e4461aa
Anthonios Partheniou <partheniou@google.com>
2024-05-29 17:03:28 +0000

    lint

--- proto/marshal/compat.py
+++ proto/marshal/compat.py
@@ -42,7 +42,7 @@ map_composite_types = (containers.MessageMap,)
 
 # In `proto/marshal.py`, for compatibility with protobuf 5.x,
 # we'll use `map_composite_type_names` to check whether
-# the name of the class of a protobuf type is 
+# the name of the class of a protobuf type is
 # `MessageMapContainer`, and, if `True`, return a MapComposite.
 # See https://github.com/protocolbuffers/protobuf/issues/16596
 map_composite_type_names = ("MessageMapContainer",)
@@ -53,7 +53,7 @@ if _message:
 
     # In `proto/marshal.py`, for compatibility with protobuf 5.x,
     # we'll use `map_composite_type_names` to check whether
-    # the name of the class of a protobuf type is 
+    # the name of the class of a protobuf type is
     # `MessageMapContainer`, and, if `True`, return a MapComposite.
     # See https://github.com/protocolbuffers/protobuf/issues/16596
     if PROTOBUF_VERSION[0:2] in ["3.", "4."]:
