7af40e3357583448ca3167b97a09668daa9c9e35
Anthonios Partheniou <partheniou@google.com>
2024-05-31 00:53:57 +0000

    fix test case

--- tests/test_message.py
+++ tests/test_message.py
@@ -256,13 +256,6 @@ def test_serialize_to_dict():
     s_dict = Squid.to_dict(s)
     assert s_dict["chromatophores"][0]["color"] == 1
 
-    s_dict = Squid.to_dict(
-        s,
-        including_default_value_fields=True,
-        always_print_fields_with_no_presence=True,
-    )
-    assert s_dict["chromatophores"][0]["color"] == 1
-
     new_s = Squid(s_dict)
     assert new_s == s
 
@@ -284,6 +277,14 @@ def test_serialize_to_dict():
         always_print_fields_with_no_presence=False,
     )
     expected_dict = {"mass_kg": 20}
+    assert s_dict_2 == expected_dict
+
+    s_dict_2 = Squid.to_dict(
+        s_new_2,
+        including_default_value_fields=True,
+        always_print_fields_with_no_presence=True,
+    )
+    expected_dict = {"mass_kg": 20, "chromatophores": []}
     assert s_dict_2 == expected_dict
 
     with pytest.raises(
